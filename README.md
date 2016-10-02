4e01f55d05d1ce1c796069fca05c4c56fc79da8c
garvin Casimir <gjcasimir@gmail.com>
2016-10-02 19:45:29 -0400

    Updating to ES 2.4.1 and new .net Azure SDK

--- README.md
+++ README.md
@@ -61,7 +61,7 @@ This is simply the name used to save the jre installer into the download cache o
 ######ElasticsearchDownloadURL
 The service will download the java jre installer from this url.
 
-**Default**: https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.0.zip
+**Default**: https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-2.4.1.zip
 
 ######ElasticsearchDownloadType
 This tells the service whether **ElasticsearchDownloadURL** is a web accessible location or on the configured storage account
@@ -72,7 +72,7 @@ This tells the service whether **ElasticsearchDownloadURL** is a web accessible 
 ######ElasticsearchZip
 This is simply the name used to save the elasticsearch package into the download cache on the role instance
 
-**Default**: elasticsearch-1.7.0.zip
+**Default**: elasticsearch-2.4.1.zip
 
 ######StorageConnection
 The service will use this connection string to download any download types marked as *storage*. It will also be used to create the share used to store elasticsearch data.
---
+++ Worker.Common/Discovery/TcpRuntimeBridge.cs
@@ -0,0 +1,87 @@
+﻿using ElasticsearchWorker.Core;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ElasticsearchWorker.Discovery
+{
+    public class TcpRuntimeBridge
+    {
+        //Unique pipe name per instance
+        private int _port;
+        private readonly string _endpointName;
+        private readonly TcpListener _server;
+        public TcpRuntimeBridge(string endpointName)
+        {
+            _endpointName = endpointName;
+            _server = new TcpListener(IPAddress.Loopback, 0);
+            _server.Start();
+
+        }
+
+        public void StartService()
+        {
+            BeginWaitForConnection(_server);
+           
+        }
+
+        public int Port { get { return ((IPEndPoint)_server.LocalEndpoint).Port; } }
+
+        private void BeginWaitForConnection(TcpListener listener)
+        {
+            // Start to listen for connections from a client.
+            Trace.TraceInformation("Runtime Bridge Start waiting for connection...");
+            listener.BeginAcceptTcpClient(new AsyncCallback(ConnectionCallback),listener);
+            
+        }
+
+        protected void ConnectionCallback(IAsyncResult ar)
+        {
+            Trace.TraceInformation("Runtime Bridge connection callback");
+            // Get the listener that handles the client request.
+            TcpListener listener = (TcpListener)ar.AsyncState;
+            // End waiting for the connection
+            TcpClient client = listener.EndAcceptTcpClient(ar);
+
+            Trace.TraceInformation("Runtime Bridge connection made");
+
+            var stream = client.GetStream();
+
+            using (var writer = new StreamWriter(stream))
+            using (var reader = new StreamReader(stream))
+            {
+                writer.AutoFlush = true;
+
+                var endpoints = from r in RoleEnvironment.Roles
+                                from i in r.Value.Instances
+                                from e in i.InstanceEndpoints
+                                where e.Key == _endpointName
+                                select new ElasticsearchNode
+                                {
+                                    Ip = e.Value.IPEndpoint.Address.ToString(),
+                                    Port = e.Value.IPEndpoint.Port,
+                                    NodeName = i.Id
+                                };
+
+                var endpointsPayload = JsonConvert.SerializeObject(endpoints);
+
+                writer.WriteLine(endpointsPayload);
+
+                Trace.TraceInformation("Writing node list: {0}", endpointsPayload);
+            }
+            // Process the connection here. (Add the client to a 
+            // server table, read data, etc.)
+            
+            Trace.TraceInformation("Node list sent! Waiting for new request.");
+            BeginWaitForConnection(listener);
+
+        }
+
+    }
+}
